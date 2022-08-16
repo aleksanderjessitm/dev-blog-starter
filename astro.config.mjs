cef2dbcde683a2bfde2e4a96485b45fc38b11e8c
Aleksander <aleksander.jess@itmagination.com>
2022-08-16 14:01:17 +0200

    changed the config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import preact from '@astrojs/preact';
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import image from "@astrojs/image";
-import vercel from '@astrojs/vercel/serverless'
+import netlify from '@astrojs/netlify/functions'
 import sitemap from '@astrojs/sitemap';
 import { config } from './config.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
   vite: {
     build: {
       reportCompressedSize: true
