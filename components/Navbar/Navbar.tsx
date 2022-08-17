3f92e20aa7640bf7059116446c5bfce77b55ef6a
Aleksander <aleksander.jess@itmagination.com>
2022-08-17 10:18:42 +0200

    added an image to the post added a post added links to the navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
@@ -31,16 +33,14 @@ export default function Navbar() {
                 <div className="navbar-collapse collapse grow items-center" id="navbarSupportedContentY">
                     <ul className="navbar-nav mr-auto lg:flex lg:flex-row">
                         <li className="nav-item">
-                            <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Home</a>
+                            <Link href="/">
+                                <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out">Home</a>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Features</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Pricing</a>
-                        </li>
-                        <li className="nav-item mb-2 lg:mb-0">
-                            <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">About</a>
+                            <Link href={"/blog"}>
+                                <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out">Blog</a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
---
+++ posts/hello-world.md
@@ -0,0 +1,9 @@
+---
+title: "Hello World"
+metaTitle: 'Hello World, Goodbye Moon'
+metaDesc: 'How to fly back from the moon to the Earth'
+socialImage: images/20220817.png
+date: '2022-08-17'
+tags:
+  - nextjs
+---
