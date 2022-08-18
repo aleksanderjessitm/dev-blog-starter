f6ac56045dd4f49ed6517b2f6133bb1e80110583
Aleksander <aleksander.jess@itmagination.com>
2022-08-18 15:13:44 +0200

    styling

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className='relative w-full flex flex-col flex-wrap text-center items-center justify-between py-3 bg-gray-900 text-gray-200 shadow-lg'>
         <p className='w-full'>Template by Aleksander Jess of <Link href="https://www.itmagination.com/"><a className='underline'>ITMAGINATION</a></Link>.</p>
-        <p className='text-sm'>Neither the author of the template, nor ITMAGINATION is not responsible for any, and all content published on this website.</p>
+        <p className='text-xs'>Neither the author of the template, nor ITMAGINATION is not responsible for any, and all content published on this website.</p>
     </footer>
   )
 }
--- components/Jumbotron/Jumbotron.tsx
+++ components/Jumbotron/Jumbotron.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 function Jumbotron({heading, subheading, buttonHref, buttonText}: Props) {
     return (
-        <div className="text-center bg-green-600 text-gray-200 py-20 px-6">
+        <div className="h-[70vh] flex flex-col items-center justify-center  text-center from-green-600 to-green-900 bg-gradient-to-b text-gray-200 py-20 px-6">
             <h1 className="text-5xl font-bold mt-0 mb-6">{heading}</h1>
             <h3 className="text-3xl font-bold mb-8">{subheading}</h3>
             <a className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href={buttonHref} role="button">{buttonText}</a>
